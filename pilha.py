428b6ba876f87b7a14aa8fbf5b41ab098cc32054
herissonsilvahs <herissonsilvahs@gmail.com>
2018-03-23 10:20:41 -0300

    update

---
+++ pilha.py
@@ -0,0 +1,20 @@
+
+class Pilha():
+    items = None
+    def __init__(self):
+        self.items = []
+
+    def push(self, element):
+        pass
+
+    def pop(self):
+        pass
+
+    def size(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def clear(self):
+        pass
